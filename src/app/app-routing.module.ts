d92fdc6f0706eb2520a23a5d8c44b6e161976a88
sergio <sergioalejandrogutierrezmedina@gmail.com>
2024-12-05 12:35:35 -0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,59 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { loadRemoteModule } from '@angular-architects/module-federation';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'inventarios',
+    loadChildren: () =>
+      loadRemoteModule({
+        type: 'module',
+        remoteEntry: 'http://localhost:4200/remoteEntry.js',
+        exposedModule: './InventariosModule',
+      }).then((m) => m.InventariosModule),
+  },
+  {
+    path: 'compras',
+    loadChildren: () =>
+      loadRemoteModule({
+        type: 'module',
+        remoteEntry: 'http://localhost:4200/remoteEntry.js',
+        exposedModule: './ComprasModule',
+      }).then((m) => m.ComprasModule),
+  },
+  {
+    path: 'pqrs-soul',
+    loadChildren: () =>
+      loadRemoteModule({
+        type: 'module',
+        remoteEntry: 'http://localhost:4200/remoteEntry.js',
+        exposedModule: './pqrs-soul',
+      }).then((m) => m.PqrsSoulModule),
+  },
+  {
+    path: 'soul-bi',
+    loadChildren: () =>
+      loadRemoteModule({
+        type: 'module',
+        remoteEntry: 'http://localhost:4200/remoteEntry.js',
+        exposedModule: './soul_bi',
+      }).then((m) => m.SoulBiModule),
+  },
+  {
+    path: 'standards',
+    loadChildren: () =>
+      loadRemoteModule({
+        type: 'module',
+        remoteEntry: 'http://localhost:4200/remoteEntry.js',
+        exposedModule: './standars',
+      }).then((m) => m.StandardsModule),
+  },
+  { path: '', redirectTo: 'inventarios', pathMatch: 'full' }, // Ruta por defecto
+  { path: '**', redirectTo: 'inventarios' }, // Ruta para no encontrados
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
